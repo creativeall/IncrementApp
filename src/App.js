0c5076ef9abe23b27a01e43f7998d19208b82523
Supriya Bhandari <153041437+creativeall@users.noreply.github.com>
2024-05-01 12:48:57 +0530

    Add files via upload
    supriya

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import AddIcon from '@mui/icons-material/Add';
+import RemoveIcon from '@mui/icons-material/Remove';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+ const App = () =>{
+  let [Count,setCount] = useState(0);
+  const increase = () =>{
+    console.log("value added",Count);
+    setCount(Count++);
+  }
+  const decrease = () =>{
+    if(Count>0){
+    setCount(Count--);
+    }
+    else{
+      alert("sorry,Zero limited reached")
+      setCount(0);
+    }
+  }
+   return<>
+   <h2>Number Game🎲</h2>
+    <div className="main_div">
+        <div className="center_div">
+            <h1>{Count}</h1>
+            <Tooltip title="Add"> 
+            <Button onClick={increase}> <AddIcon /></Button>
+            </Tooltip>
+            <Tooltip title="Delete"> 
+            <Button onClick={decrease}> <RemoveIcon/></Button>
+            </Tooltip>
+        </div>
     </div>
-  );
-}
-
-export default App;
+   </>
+ }
+ export default App;
